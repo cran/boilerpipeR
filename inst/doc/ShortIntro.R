263b4b304db1a4169655e696e58ec329d8fb9f95
Mario Annau <mario.annau@gmail.com>
2021-05-19 08:20:02 +0000

    version 1.3.2

--- inst/doc/ShortIntro.R
+++ inst/doc/ShortIntro.R
@@ -9,34 +9,34 @@ options(width = 60)
 
 
 ###################################################
-### code chunk number 2: ShortIntro.Rnw:56-58 (eval = FALSE)
+### code chunk number 2: ShortIntro.Rnw:57-59 (eval = FALSE)
 ###################################################
 ## library(boilerpipeR)
 ## library(RCurl)
 
 
 ###################################################
-### code chunk number 3: ShortIntro.Rnw:61-63 (eval = FALSE)
+### code chunk number 3: ShortIntro.Rnw:62-64 (eval = FALSE)
 ###################################################
-## url <- "http://quantivity.wordpress.com/2012/11/09/multi-asset-market-regimes/"
+## url <- "https://quantivity.wordpress.com/2012/11/09/multi-asset-market-regimes/"
 ## content <- getURL(url)
 
 
 ###################################################
-### code chunk number 4: ShortIntro.Rnw:71-72
+### code chunk number 4: ShortIntro.Rnw:72-73
 ###################################################
 cat(substr(content, 1, 80))
 
 
 ###################################################
-### code chunk number 5: ShortIntro.Rnw:94-96
+### code chunk number 5: ShortIntro.Rnw:95-97
 ###################################################
 extract <- DefaultExtractor(content)
 cat(substr(extract, 1, 120))
 
 
 ###################################################
-### code chunk number 6: ShortIntro.Rnw:120-127
+### code chunk number 6: ShortIntro.Rnw:121-128
 ###################################################
 articleextract <- ArticleExtractor(content)
 articlesentencesextract <- ArticleSentencesExtractor(content)
